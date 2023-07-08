c362d7afe566eb36838377698f3fc2c5c2fcec7f
maks <maxim_86nva@mail.ru>
2023-07-08 12:34:48 +0300

    созданы классы Book и Author + конструкторы, геттеры и сеттеры для них

--- src/com/hw/maxim/Main.java
+++ src/com/hw/maxim/Main.java
@@ -3,6 +3,20 @@ package com.hw.maxim;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        Author pushkin = new Author("Александр", "Пушкин");
+        Book kapitanskayaDochka = new Book("Капитанская дочка", pushkin, 1836);
+
+        Author lermontov = new Author("Михаил", "Лермонтов");
+        Book geroyNashegoVremeny = new Book("Герой нашего времени", lermontov, 1839);
+
+        System.out.println(kapitanskayaDochka.getName());
+        System.out.println(kapitanskayaDochka.getYear());
+        kapitanskayaDochka.setYear(1835);
+        System.out.println(kapitanskayaDochka.getYear());
+
+        System.out.println(geroyNashegoVremeny.getName());
+        System.out.println(geroyNashegoVremeny.getYear());
+        geroyNashegoVremeny.setYear(1800);
+        System.out.println(geroyNashegoVremeny.getYear());
     }
 }
