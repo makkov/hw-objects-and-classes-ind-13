2117f143b395685ae4f8ddf8e21d25cdb65dedda
maks <maxim_86nva@mail.ru>
2023-07-08 13:13:36 +0300

    переопределелили методы toString, equals, hashCode

--- src/com/hw/maxim/objects/and/classes/Author.java
+++ src/com/hw/maxim/objects/and/classes/Author.java
@@ -1,5 +1,7 @@
 package com.hw.maxim.objects.and.classes;
 
+import java.util.Objects;
+
 public class Author {
 
     private String firstName;
@@ -17,5 +19,26 @@ public class Author {
 
     public String getLastName() {
         return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author)) return false;
+        Author author = (Author) o;
+        return Objects.equals(getFirstName(), author.getFirstName()) && Objects.equals(getLastName(), author.getLastName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirstName(), getLastName());
     }
 }
--- src/com/hw/maxim/objects/and/classes/Main.java
+++ src/com/hw/maxim/objects/and/classes/Main.java
@@ -9,10 +9,20 @@ public class Main {
         Author lermontov = new Author("Михаил", "Лермонтов");
         Book geroyNashegoVremeny = new Book("Герой нашего времени", lermontov, 1839);
 
-        System.out.println(kapitanskayaDochka.getName());
-        System.out.println(kapitanskayaDochka.getYear());
+//        System.out.println(kapitanskayaDochka.getName());
+//        System.out.println(kapitanskayaDochka.getYear());
+//
+//        System.out.println(geroyNashegoVremeny.getName());
+//        System.out.println(geroyNashegoVremeny.getYear());
 
-        System.out.println(geroyNashegoVremeny.getName());
-        System.out.println(geroyNashegoVremeny.getYear());
+//        System.out.println(pushkin);
+        System.out.println(kapitanskayaDochka);
+
+//        Author pushkin2 = new Author("Александр", "Пушкин");
+//
+//
+//        System.out.println(pushkin.equals(pushkin2));
+//        System.out.println(pushkin.hashCode());
+//        System.out.println(pushkin2.hashCode());
     }
 }
